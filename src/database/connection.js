fe37b8b93d7cd5accca0c1dc9ae6efc975ae8e1a
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-04-26 07:37:40 +0530

    updated

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const { DB_URL } = require("../config/env");
-mongoose.connect(DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true})
+mongoose.connect(DB_URL)
 .then(()=>{console.log("connection successful")})
 .catch((err)=>{console.log(err)})
