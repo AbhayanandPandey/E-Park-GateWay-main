8cbcdbd79f6fabe0787bea4a46e6b497bf60f13b
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-04-26 06:38:44 +0530

    updated

--- src/config/env.js
+++ src/config/env.js
@@ -2,5 +2,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   PORT: process.env.PORT || 3000,
-  DB_URL: process.env.DB_URL || "mongodb+srv://eparkgateway:qsb3O2PbQzMUysvX@epark.vrqna2i.mongodb.net/NEWDATA?retryWrites=true&w=majority&appName=Epark",
+  DB_URL: process.env.DB_URL || "mongodb+srv://iamabhayanand1:n8ylLGy3sfeaZBZX@e-park-gateway.0urvfam.mongodb.net/NEWDATA?retryWrites=true&w=majority&appName=e-park-gateway",
 };
