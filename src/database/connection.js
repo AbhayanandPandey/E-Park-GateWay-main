6d16f588e3eec7059ee00543e67a8cb135639871
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-04-26 06:25:05 +0530

    updated

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 const { DB_URL } = require("../config/env");
-mongoose.connect(DB_URL||"mongodb+srv://eparkgateway:qsb3O2PbQzMUysvX@epark.vrqna2i.mongodb.net/NEWDATA?retryWrites=true&w=majority&appName=Epark")
+mongoose.connect(DB_URL)
 .then(()=>{console.log("connection successful")})
 .catch((err)=>{console.log(err)})
